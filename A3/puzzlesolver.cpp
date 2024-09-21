d10972a2b91268b9fe64dd498ca2885d4702bcd1
LovisaBa <lovisa@LAPTOP-LOVISA>
2024-09-21 11:56:44 +0000

    first evil message sent

--- A3/puzzlesolver.cpp
+++ A3/puzzlesolver.cpp
@@ -12,13 +12,15 @@
 #include <netinet/udp.h>
 #include <netdb.h>
 
-std::pair<int, struct sockaddr_in> connect_to_port(const std::string &addr, int port)
+using namespace std;
+
+pair<int, struct sockaddr_in> connect_to_port(const string &addr, int port)
 {
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0)
     {
         perror("Error creating socket.");
-        return std::make_pair(-1, sockaddr_in());
+        return make_pair(-1, sockaddr_in());
     }
 
     struct sockaddr_in server_addr;
@@ -27,9 +29,9 @@ std::pair<int, struct sockaddr_in> connect_to_port(const std::string &addr, int 
 
     if (inet_pton(AF_INET, addr.c_str(), &server_addr.sin_addr) <= 0)
     {
-        std::cout << "Unable to set IP address" << std::endl;
+        cout << "Unable to set IP address" << endl;
         close(sockfd);
-        return std::make_pair(-1, sockaddr_in());
+        return make_pair(-1, sockaddr_in());
     }
 
     // Set up a timeout for the socket
@@ -38,12 +40,12 @@ std::pair<int, struct sockaddr_in> connect_to_port(const std::string &addr, int 
     timeout.tv_usec = 0;
     setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&timeout, sizeof(timeout));
 
-    return std::make_pair(sockfd, server_addr);
+    return make_pair(sockfd, server_addr);
 }
 
-std::string check_port(const std::string &addr, int port)
+string check_port(const string &addr, int port)
 {
-    std::pair<int, struct sockaddr_in> connection = connect_to_port(addr, port);
+    pair<int, struct sockaddr_in> connection = connect_to_port(addr, port);
     int sockfd = connection.first;
     struct sockaddr_in server_addr = connection.second;
     if (sockfd < 0)
@@ -60,7 +62,7 @@ std::string check_port(const std::string &addr, int port)
         // send a message to the port
         if (sendto(sockfd, "hello?", 7, 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
         {
-            std::cerr << "Failed to send message to IP address." << std::endl;
+            cerr << "Failed to send message to IP address." << endl;
             close(sockfd);
             return "";
         }
@@ -81,17 +83,17 @@ std::string check_port(const std::string &addr, int port)
     return "";
 }
 
-std::string sort_port(std::string portmsg)
+string sort_port(string portmsg)
 {
-    if (portmsg.find("Enhanced X-link Port Storage Transaction Node") != std::string::npos)
+    if (portmsg.find("Enhanced X-link Port Storage Transaction Node") != string::npos)
     {
         return "expstn";
     }
-    else if (portmsg.find("Secure Encryption Certification Relay with Enhanced Trust") != std::string::npos)
+    else if (portmsg.find("Secure Encryption Certification Relay with Enhanced Trust") != string::npos)
     {
         return "secret";
     }
-    else if (portmsg.find("https://en.wikipedia.org/wiki/Evil_bit") != std::string::npos)
+    else if (portmsg.find("https://en.wikipedia.org/wiki/Evil_bit") != string::npos)
     {
         return "dark";
     }
@@ -101,22 +103,22 @@ std::string sort_port(std::string portmsg)
     }
 }
 
-std::pair<std::string, int> check_and_sort_port(const std::string &ip_addr, int port)
+pair<string, int> check_and_sort_port(const string &ip_addr, int port)
 {
-    std::string port_msg = check_port(ip_addr, port);
-    std::string port_type = sort_port(port_msg);
-    return std::make_pair(port_type, port);
+    string port_msg = check_port(ip_addr, port);
+    string port_type = sort_port(port_msg);
+    return make_pair(port_type, port);
 }
 
-std::pair<int, int> solve_secret_port(const std::string &addr, int port)
+pair<int, int> solve_secret_port(const string &addr, int port)
 // Greetings from S.E.C.R.E.T (Secure Encryption Certification Relay with Enhanced Trust)! Here's how to access the secret port I'm safeguarding:
 {
-    std::pair<int, struct sockaddr_in> connection = connect_to_port(addr, port);
+    pair<int, struct sockaddr_in> connection = connect_to_port(addr, port);
     int sockfd = connection.first;
     struct sockaddr_in server_addr = connection.second;
     if (sockfd < 0)
     {
-        return std::make_pair(-1, -1);
+        return make_pair(-1, -1);
     }
     char buffer[1024];
     int attempts = 0;
@@ -129,9 +131,9 @@ std::pair<int, int> solve_secret_port(const std::string &addr, int port)
         //  1. Send me your group number as a single unsigned byte.
         if (sendto(sockfd, &group_nr, sizeof(group_nr), 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
         {
-            std::cerr << "Failed to send message to IP address." << std::endl;
+            cerr << "Failed to send message to IP address." << endl;
             close(sockfd);
-            return std::make_pair(-1, -1);
+            return make_pair(-1, -1);
         }
 
         // Wait for a response.
@@ -153,7 +155,7 @@ std::pair<int, int> solve_secret_port(const std::string &addr, int port)
             uint32_t network_order_four_byte = htonl(signature);
             uint8_t message[5];
             message[0] = group_nr;
-            std::memcpy(&message[1], &network_order_four_byte, sizeof(network_order_four_byte));
+            memcpy(&message[1], &network_order_four_byte, sizeof(network_order_four_byte));
 
             // Attempt to send the signed challenge and receive the response up to 5 times
             int inner_attempts = 0;
@@ -162,31 +164,31 @@ std::pair<int, int> solve_secret_port(const std::string &addr, int port)
                 // Send the 5-byte message
                 if (sendto(sockfd, message, sizeof(message), 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
                 {
-                    std::cerr << "Failed to send signed challenge to IP address." << std::endl;
+                    cerr << "Failed to send signed challenge to IP address." << endl;
                     close(sockfd);
-                    return std::make_pair(-1, -1);
+                    return make_pair(-1, -1);
                 }
-                std::memset(buffer, 0, sizeof(buffer));
+                memset(buffer, 0, sizeof(buffer));
 
                 //  5. If your signature is correct, I'll grant you access to the port. Good luck!
                 if (recvfrom(sockfd, buffer, sizeof(buffer), 0, NULL, NULL) >= 0)
                 {
                     close(sockfd);
                     // Extract the port number from the buffer
-                    std::string response(buffer);
+                    string response(buffer);
                     size_t pos = response.find_last_of(':');
-                    if (pos != std::string::npos)
+                    if (pos != string::npos)
                     {
-                        std::string number_str = response.substr(pos + 2, 4);
-                        int extracted_port = std::stoi(number_str);
-                        return std::make_pair(extracted_port, signature);
+                        string number_str = response.substr(pos + 2, 4);
+                        int extracted_port = stoi(number_str);
+                        return make_pair(extracted_port, signature);
                     }
                 }
 
                 // try again if failed
                 ++inner_attempts;
             }
-            return std::make_pair(-1, -1);
+            return make_pair(-1, -1);
         }
 
         // try again if failed
@@ -196,12 +198,12 @@ std::pair<int, int> solve_secret_port(const std::string &addr, int port)
     // All 5 attempts have failed, return false
     close(sockfd);
 
-    return std::make_pair(-1, -1);
+    return make_pair(-1, -1);
 }
 
-bool solve_checksum_port(const std::string &addr, int port, uint32_t secret)
+bool solve_checksum_port(const string &addr, int port, uint32_t secret)
 {
-    std::pair<int, struct sockaddr_in> connection = connect_to_port(addr, port);
+    pair<int, struct sockaddr_in> connection = connect_to_port(addr, port);
     int sockfd = connection.first;
     struct sockaddr_in server_addr = connection.second;
     if (sockfd < 0)
@@ -221,27 +223,27 @@ bool solve_checksum_port(const std::string &addr, int port, uint32_t secret)
         // Send me a 4-byte message containing the signature you got from S.E.C.R.E.T in the first 4 bytes (in network byte order).
         if (sendto(sockfd, &message, sizeof(message), 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
         {
-            std::cerr << "Failed to send message to IP address first." << std::endl;
+            cerr << "Failed to send message to IP address first." << endl;
             close(sockfd);
             return false;
         }
 
-        std::memset(buffer, 0, sizeof(buffer));
+        memset(buffer, 0, sizeof(buffer));
         // Wait for a response. if there is a response, the port is open
         if (recvfrom(sockfd, buffer, sizeof(buffer), 0, NULL, NULL) < 0)
         {
             return false;
         }
-        std::cout << buffer << std::endl;
+        cout << buffer << endl;
         int checksum;
-        std::string source_address, information;
-        std::string response(buffer);
+        string source_address, information;
+        string response(buffer);
 
         size_t pos = response.find("0x");
-        if (pos != std::string::npos)
+        if (pos != string::npos)
         {
-            std::string number_str = response.substr(pos + 2, 6);
-            checksum = std::stoi(number_str, nullptr, 16);
+            string number_str = response.substr(pos + 2, 6);
+            checksum = stoi(number_str, nullptr, 16);
         }
         size_t start_pos = response.find("being");
         start_pos += 6;
@@ -307,11 +309,44 @@ unsigned short csum(unsigned short *ptr, int nbytes)
     return (answer);
 }
 
-bool solve_dark_port(const std::string &addr, int port, int secret)
+pair<string, int> get_source_ip_and_port(int sockfd, struct sockaddr_in server_addr)
+{
+    if (sockfd < 0)
+    {
+        return make_pair("", -1);
+    }
+
+    // Ensure the socket is connected
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        close(sockfd);
+        return make_pair("", -1);
+    }
+
+    // Get and print the local address and port
+    struct sockaddr_in local_addr;
+    socklen_t addr_len = sizeof(local_addr);
+    if (getsockname(sockfd, (struct sockaddr *)&local_addr, &addr_len) == 0)
+    {
+        return make_pair(inet_ntoa(local_addr.sin_addr), ntohs(local_addr.sin_port));
+    }
+    else
+    {
+        return make_pair("", -1);
+    }
+}
+
+bool solve_dark_port(const string &addr, int port, int secret)
 // todo
 // The dark side of network programming is a pathway to many abilities some consider to be...unnatural. I am an evil port, I will only communicate with evil processes! (https://en.wikipedia.org/wiki/Evil_bit)
 // Send us a message of 4 bytes containing the signature that you created with S.E.C.R.E.T
 {
+    pair<int, struct sockaddr_in> connection = connect_to_port(addr, port);
+    int udp_socket = connection.first;
+    struct sockaddr_in server_addr = connection.second;
+    pair<string, int> source_ip_and_port = get_source_ip_and_port(udp_socket, server_addr);
+    string source_ip = source_ip_and_port.first;
+    int source_port = source_ip_and_port.second;
     int s = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
     if (s < 0)
     {
@@ -338,31 +373,8 @@ bool solve_dark_port(const std::string &addr, int port, int secret)
         return false;
     }
 
-    // Resolve local IP address dynamically
-    char hostbuffer[256];
-    char *IPbuffer;
-    struct hostent *host_entry;
-
-    // Retrieve the hostname
-    if (gethostname(hostbuffer, sizeof(hostbuffer)) == -1)
-    {
-        perror("Error getting local hostname.");
-        return false;
-    }
-
-    // Retrieve host information
-    host_entry = gethostbyname(hostbuffer);
-    if (!host_entry)
-    {
-        perror("Error getting host info.");
-        return false;
-    }
-
-    // Convert to IP string
-    IPbuffer = inet_ntoa(*((struct in_addr *)host_entry->h_addr_list[0]));
-
     // Datagram to represent the packet
-    char datagram[4096], source_ip[32], *data, *pseudogram;
+    char datagram[4096], *data, *pseudogram;
 
     // zero out the packet buffer
     memset(datagram, 0, 4096);
@@ -378,12 +390,10 @@ bool solve_dark_port(const std::string &addr, int port, int secret)
 
     // Data part
     // data = datagram + sizeof(struct iphdr) + sizeof(struct udphdr);
-    // std::string secret_str = std::to_string(secret);
+    // string secret_str = to_string(secret);
     // strcpy(data, secret_str.c_str());
     data = datagram + sizeof(struct iphdr) + sizeof(struct udphdr);
     strcpy(data, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-
-    strcpy(source_ip, IPbuffer);
 
     sin.sin_family = AF_INET;
     sin.sin_port = htons(port);
@@ -398,33 +408,21 @@ bool solve_dark_port(const std::string &addr, int port, int secret)
     iph->frag_off = 0x80;   // evil bit?
     iph->ttl = 255;
     iph->protocol = IPPROTO_UDP;
-    iph->check = 0;                   // Set to 0 before calculating checksum
-    iph->saddr = inet_addr(IPbuffer); // Dynamically set the local IP
+    iph->check = 0; // Set to 0 before calculating checksum
+    iph->saddr = inet_addr(source_ip.c_str());
     iph->daddr = sin.sin_addr.s_addr;
 
     // Ip checksum
     iph->check = csum((unsigned short *)datagram, iph->tot_len);
 
     // UDP header
-    // NOTE: fix?
-    // Now connect the socket to find out the assigned port
-    // struct sockaddr_in local_addr;
-    // socklen_t addr_len = sizeof(local_addr);
-    // if (getsockname(s, (struct sockaddr *)&local_addr, &addr_len) == -1)
-    // {
-    //     perror("Error getting socket name.");
-    //     return false;
-    // }
-
-    // // Update source port in the UDP header with the dynamically assigned port
-    // udph->source = local_addr.sin_port;
-    udph->source = htons(0); // Let the OS assign the source port dynamically
+    udph->source = htons(source_port); // Let the OS assign the source port dynamically
     udph->dest = htons(port);
     udph->len = htons(8 + strlen(data)); // UDP header size
     udph->check = 0;                     // leave checksum 0 now, filled later by pseudo header
 
     // Now the UDP checksum using the pseudo header
-    psh.source_address = inet_addr(IPbuffer); // Dynamically set the source IP
+    psh.source_address = inet_addr(source_ip.c_str()); // Dynamically set the source IP
     psh.dest_address = sin.sin_addr.s_addr;
     psh.placeholder = 0;
     psh.protocol = IPPROTO_UDP;
@@ -443,7 +441,7 @@ bool solve_dark_port(const std::string &addr, int port, int secret)
 
     while (attempts < max_retries)
     {
-        // std::cout << "Datagram: " << datagram << std::endl;
+        // cout << "Datagram: " << datagram << endl;
         // Send the packet
         if (sendto(s, datagram, iph->tot_len, 0, (struct sockaddr *)&sin, sizeof(sin)) < 0)
         {
@@ -453,18 +451,18 @@ bool solve_dark_port(const std::string &addr, int port, int secret)
         else
         {
             char buffer[1024];
-            std::memset(buffer, 0, sizeof(buffer));
+            memset(buffer, 0, sizeof(buffer));
 
             // Check if a response is received
-            if (recvfrom(s, buffer, sizeof(buffer), 0, NULL, NULL) >= 0)
+            if (recvfrom(udp_socket, buffer, sizeof(buffer), 0, NULL, NULL) >= 0)
             {
-                std::cout << buffer << std::endl;
+                cout << buffer << endl;
                 close(s);
                 return true;
             }
             else
             {
-                std::cout << "No response received." << std::endl;
+                cout << "No response received." << endl;
             }
         }
         attempts++;
@@ -474,7 +472,7 @@ bool solve_dark_port(const std::string &addr, int port, int secret)
     return false;
 }
 
-bool solve_expstn_port(const std::string &addr, int port)
+bool solve_expstn_port(const string &addr, int port)
 // todo
 // Greetings! I am E.X.P.S.T.N, which stands for "Enhanced X-link Port Storage Transaction Node".
 // What can I do for you?
@@ -487,12 +485,12 @@ bool solve_expstn_port(const std::string &addr, int port)
     return false;
 }
 
-bool solve_secret_secret_port(const std::string &addr, int port, std::string ports)
+bool solve_secret_secret_port(const string &addr, int port, string ports)
 // todo
 {
     return false;
-    // std::cout << ports << std::endl;
-    // std::pair<int, struct sockaddr_in> connection = connect_to_port(addr, port);
+    // cout << ports << endl;
+    // pair<int, struct sockaddr_in> connection = connect_to_port(addr, port);
     // int sockfd = connection.first;
     // struct sockaddr_in server_addr = connection.second;
     // if (sockfd < 0)
@@ -509,7 +507,7 @@ bool solve_secret_secret_port(const std::string &addr, int port, std::string por
     //     // send a message to the port
     //     if (sendto(sockfd, "hi", 3, 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     //     {
-    //         std::cerr << "Failed to send message to IP address." << std::endl;
+    //         cerr << "Failed to send message to IP address." << endl;
     //         close(sockfd);
     //         return "";
     //     }
@@ -518,7 +516,7 @@ bool solve_secret_secret_port(const std::string &addr, int port, std::string por
     //     if (recvfrom(sockfd, buffer, sizeof(buffer), 0, NULL, NULL) >= 0)
     //     {
     //         close(sockfd);
-    //         std::cout << buffer << std::endl;
+    //         cout << buffer << endl;
     //         return true;
     //     }
 
@@ -540,15 +538,15 @@ int main(int argc, char *argv[])
         exit(0);
     }
 
-    std::string ip_addr = argv[1];
+    string ip_addr = argv[1];
     int port1 = atoi(argv[2]);
     int port2 = atoi(argv[3]);
     int port3 = atoi(argv[4]);
     int port4 = atoi(argv[5]);
 
     int secret_port, dark_port, checksum_port, expstn_port;
-    std::string ports;
-    ports = std::to_string(port1) + "," + std::to_string(port2) + "," + std::to_string(port3) + "," + std::to_string(port4);
+    string ports;
+    ports = to_string(port1) + "," + to_string(port2) + "," + to_string(port3) + "," + to_string(port4);
 
     auto port1_result = check_and_sort_port(ip_addr, port1);
     auto port2_result = check_and_sort_port(ip_addr, port2);
@@ -594,7 +592,7 @@ int main(int argc, char *argv[])
     auto secret_response = solve_secret_port(ip_addr, secret_port);
     if (secret_response.first == -1 || secret_response.second == -1)
     {
-        std::cout << "Failed to solve secret port." << std::endl;
+        cout << "Failed to solve secret port." << endl;
         return -1;
     }
 
